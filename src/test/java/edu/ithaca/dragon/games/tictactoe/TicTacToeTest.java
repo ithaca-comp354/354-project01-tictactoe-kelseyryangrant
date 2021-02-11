76ff0f5ad6bbb2776f938dae6e16d6f2ad57c507
kelseygrant <kgrant5@ithaca.edu>
2021-02-11 14:41:34 -0500

    small name changes

--- src/test/java/edu/ithaca/dragon/games/tictactoe/TicTacToeTest.java
+++ src/test/java/edu/ithaca/dragon/games/tictactoe/TicTacToeTest.java
@@ -7,14 +7,14 @@ import org.junit.jupiter.api.Test;
 import edu.ithaca.dragon.games.tictactoe.player.BrainDeadPlayer;
 import edu.ithaca.dragon.games.tictactoe.player.HumanPlayer;
 //import edu.ithaca.dragon.games.tictactoe.player.NewPlayer;
-import edu.ithaca.dragon.games.tictactoe.player.SecondPlayer;
+import edu.ithaca.dragon.games.tictactoe.player.RuleBasedAgentKelsey;
 //import edu.ithaca.dragon.games.tictactoe.player.ThirdPlayer;
 
 public class TicTacToeTest {
 
     @Test
     public void brainDeadGameTest(){
-        TicTacToeGame game = new TicTacToeGame(new SecondPlayer(), new HumanPlayer());
+        TicTacToeGame game = new TicTacToeGame(new RuleBasedAgentKelsey(), new HumanPlayer());
         while (game.calcGameStatus() == GameStatus.PLAYING){
             game.takeTurn();
         }
